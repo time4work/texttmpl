f008836597e23106f4a95ff303f2bc6d0476c427
Sad Man <m.onevegas@gmail.com>
2017-11-16 13:13:27 +0100

    up

--- generator.js
+++ generator.js
@@ -1,42 +1,47 @@
 var fs = require('fs');
-// var obj = JSON.parse(fs.readFileSync('template.json', 'utf8'));
 var obj = JSON.parse(fs.readFileSync('template2.json', 'utf8'));
 // console.log(obj);
 
-
-var who = rand(obj.who.adjective)+" "
-	+rand(obj.who.noun)+" "
-	+rand(obj.who.nationality)+" "
-	+rand(obj.who.type)+" "
-	+rand(obj.who.name);
-// var where = rand(obj.where.time)+" "+rand(obj.where.location);
-var condition = rand(obj.condition.do)+" "
-	+rand(obj.condition.noun)+" "
-	+rand(obj.condition.adjective)+" "
-	+rand(obj.condition.and)+" "
-	+rand(obj.condition.do2)+" "
-	+rand(obj.condition.event);
-
-var consequent = rand(obj.consequent.result);
-
-// console.log(who+" "+where+" "+condition+" "+consequent);
-console.log(who+" "+condition+". "+consequent);
+var res = parse( obj['start'], obj );
+console.log(res);
+console.log();
 
 
+function parse(e, obj){
+	var regexp = /<\w*>/ig;
 
+	console.log('- e:');
+	console.log(e);
+	console.log('-----');
+
+	if ( Array.isArray(e) ){
+		return parse( rand(e), obj );
+	}
+	else{
+		if( /<\w*>/i.test(e) ){
+			var result = '';
+			var last_pos = 0;
+			while ( foo = regexp.exec(e)) {
+
+				result += e.substring(last_pos,foo.index);
+				
+				var ind = foo[0].replace(/[<>]*/g,'');
+				result += parse( obj[ind], obj );
+
+				last_pos = regexp.lastIndex;
+			}
+			return result;
+		}else{
+			return e;
+		}
+	}
+}
 function rand(items){
     return items[~~(Math.random() * items.length)];
     // return items[Math.floor(Math.random()*items.length)];
 }
-// var text = '';
 // var arr = [who,where,condition];
 // arr = shuffle(arr);
-
-// for(var i in arr){
-// 	text += arr[i]+" "
-// }
-// text += consequent
-// console.log(text);
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
   while (0 !== currentIndex) {
